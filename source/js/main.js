ffc498077fc4d347eb26e586c8c5fc19663fba95
Kostikov Mihail <kostikovmu@ya.ru>
2018-10-12 17:52:05 +0300

    section projects on page 2 was added

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
   $('.slider').slick({    
-    infinite: false,
+    infinite: true,
     slidesToShow: 1,
-    prevArrow: $('.hero__btn-lft'),
-    nextArrow: $('.hero__btn-rght')
+    prevArrow: $('.btn-lft'),
+    nextArrow: $('.btn-rght')
   });
 
   $('.slider').on('afterChange', function(event, slick, currrentSlide, nextSlide){
-
     $('.hero__slider-counter').text(currrentSlide + 1 + '/3');
     console.log(currrentSlide);
   })
+
+  $('.s-projects__carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.btn-lft'),
+    nextArrow: $('.btn-rght')
+  });
+ 
 })
